5d5ec20752124e40e1ced85385a8110f2de7d5c0
AgarwalMayank2 <mayankagarwal020905@gmail.com>
2025-10-21 01:54:05 +0530

    MVT

--- my_django_project/app_create/models.py
+++ my_django_project/app_create/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class basic_details(models.Model):
+    full_name = models.CharField(max_length=200)
+    mobile_number = models.IntegerField()
+    email_id = models.CharField(max_length=200)
